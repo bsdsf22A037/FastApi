4080b538565b2185c5cd11b90e1e3d46116ac882
Muhammad Farhan <bsdsf22a037@pucit.edu.pk>
2025-06-15 02:34:09 -0400

    User class

---
+++ venv/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Boolean,TIMESTAMP,text
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)  
+    password = Column(String, nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
