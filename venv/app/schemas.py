046e0f150072dfc79c5394a3a3d13553af003cde
Muhammad Farhan <bsdsf22a037@pucit.edu.pk>
2025-06-15 02:33:15 -0400

    pydantic model to manage request and response

--- venv/app/schemas.py
+++ venv/app/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 
 #only fields we want in reponse
 
@@ -7,6 +7,18 @@ class Post_response(BaseModel):
     title: str
     content: str
     published: bool
+
+    model_config = {
+        "from_attributes": True
+    }
+
+class UserCreate(BaseModel):
+    email : EmailStr
+    password : str
+
+class UserOut(BaseModel):
+    id : int
+    email : EmailStr
 
     model_config = {
         "from_attributes": True
