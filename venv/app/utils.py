527d5ac483e39d880b86e6ea776927c464b30399
Muhammad Farhan <bsdsf22a037@pucit.edu.pk>
2025-06-24 07:18:09 -0400

    authenticating the password

--- venv/app/utils.py
+++ venv/app/utils.py
@@ -4,3 +4,8 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 #hashing the password
 def hash(password:str):
     return pwd_context.hash(password)
+
+#comparing the password 
+
+def verify(plain_password,hashed_password):
+    return pwd_context.verify(plain_password,hashed_password)
